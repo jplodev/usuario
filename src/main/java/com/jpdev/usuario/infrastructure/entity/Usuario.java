099ca2364735e9937bd5513047250e3134011cb0
João Paulo Lima <jplopapa.dev@gmail.com>
2025-08-26 12:07:52 -0300

    Adicionado métodos Login, Get e Delete de usuário

--- src/main/java/com/jpdev/usuario/infrastructure/entity/Usuario.java
+++ src/main/java/com/jpdev/usuario/infrastructure/entity/Usuario.java
@@ -2,7 +2,10 @@ package com.jpdev.usuario.infrastructure.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @NoArgsConstructor
@@ -12,7 +15,7 @@ import java.util.List;
 @Builder
 @Entity
 @Table(name = "usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +33,18 @@ public class Usuario {
     @JoinColumn(name = "usuario_id", referencedColumnName = "id")
     private List<Telefone> telefones;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
