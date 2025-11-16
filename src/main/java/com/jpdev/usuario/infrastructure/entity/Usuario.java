3fb71f10c7870ae1f63a49599538f5774a196a80
João Paulo Lima <jplopapa.dev@gmail.com>
2025-11-16 17:09:46 -0300

    Adicionadas classes de secority

---
+++ src/main/java/com/jpdev/usuario/infrastructure/entity/Usuario.java
@@ -0,0 +1,34 @@
+package com.jpdev.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome", length = 100)
+    private String nome;
+    @Column(name = "email", length = 100)
+    private String email;
+    @Column(name = "senha")
+    private String senha;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<Endereco> enderecos;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<Telefone> telefones;
+}
---
+++ src/main/java/com/jpdev/usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,20 @@
+package com.jpdev.usuario.infrastructure.repository;
+
+import com.jpdev.usuario.infrastructure.entity.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.expression.spel.ast.OpLE;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
